5a5a7c0eda187350b36f60580ad5d7c16b245009
kageyama <merveilles19980318@yahoo.co.jp>
2015-11-09 22:26:38 +0900

    update

---
+++ .bash_profile
@@ -0,0 +1,4 @@
+alias vi=vim
+
+export TERM=xterm-256color
+export LC_ALL=en_US.UTF-8
--- link.sh
+++ link.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-ln -fs ~/dotfiles/.vimrc ~/.vimrc
-ln -fs ~/dotfiles/colors/molokai.vim ~/.vim/colors/molokai.vim
+# conf
+ln -fs ./.vimrc ~/.vimrc
+ln -fs ./.bash_profile ~/.bash_profile
+ln -fs ./.gemrc ~/.gemrc
+
+# color
+ln -fs ./colors/molokai.vim ~/.vim/colors/molokai.vim
 
