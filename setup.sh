4887c3c7d4f1fa50c6a64f83fcf8d8b87a465785
miyaviee <merveilles19980318@yahoo.co.jp>
2016-11-18 16:25:20 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -9,27 +9,22 @@ if [ ! -e ~/.vim/autoload/plug.vim ]; then
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-# conf
-for dotfile in .??*
-do
-  if [ -d $dotfile ]; then
-    continue;
-  fi
-  if [ $dotfile = ".gitignore" ]; then
-    continue;
-  fi
-  ln -fs `pwd`/$dotfile ~/$dotfile
-done
-
 # filetype conf
 for name in *
 do
+  if [[ "$name" =~ ^.git*$ ]]; then
+    continue;
+  fi
   if [ -d $name ]; then
     mkdir -p ~/.vim/$name
     for file in $name/*
     do
       ln -fs `pwd`/$file ~/.vim/$file
     done
+    continue;
+  fi
+  if [[ "$name" =~ ^\.*$ ]]; then
+    ln -fs `pwd`/$name ~/$name
   fi
 done
 
@@ -37,8 +32,11 @@ vim +:PlugInstall +:q vim +:PlugClean +:q +:q
 vim install.go +:GoInstallBinaries +:q
 
 # del dead link
-for link in ~/.vimrc*
+for link in ~/.*
 do
+  if [ ! -L $link ]; then
+    continue;
+  fi
   if [ -e "`readlink $link`" ]; then
     continue;
   fi
